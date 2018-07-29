31bbc008edf37ac8eb8524bf0aa3c6c68c7444d7
Marin Bizov <34997944+mbizov@users.noreply.github.com>
2018-07-29 10:13:37 +0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 REM If this script doesn't work try compiling with Visual Studio.
 @echo off
 mkdir bin
-gcc.exe -O3 -lws2_32.lib src/*.c -o bin/tcpsniff.exe
+gcc.exe -O3 -lws2_32 src/*.c -o bin/tcpsniff.exe -static-libgcc
 strip.exe bin/tcpsniff.exe
